14b420f7e7f2579bd87780f8cd4b0dd501ba6a87
Caio Vinicius Ourique Loureiro <caio.loureiro@itajai.sc.gov.br>
2024-11-12 16:36:28 -0300

    update

--- view/introducao.php
+++ view/introducao.php
@@ -2,13 +2,12 @@
 	<div class="txt02">
 Divino Jesus, <br/>
 nós Vos oferecemos este terço que vamos rezar, <br/>
-meditando nos mistérios da Vossa Redenção. <br/>
+meditando nos mistérios da nossa Redenção. <br/>
 Concedei-nos, <br/>
-por intercessão da Virgem Maria, <br/>
-Mãe de Deus e nossa Mãe, <br/>
-as virtudes que nos são necessárias <br/>
+por intercessão de Vossa Mãe Santíssima, <br/>
+a quem me dirijo, as virtudes que nos são necessárias <br/>
 para bem rezá-lo e a graça de ganharmos <br/>
-as indulgências desta santa devoção.
+as indulgências anexas a esta  santa devoção.
 	</div>
 	<div class="btn">Avançar <span class="seta"></span></div>
 </div>
